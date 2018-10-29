eecf4c6a484bdcc892cf7eb187c470356bbe2fe5
Ariel Hicks <hicks.ariel@gmail.com>
2018-10-29 13:49:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  console.log(`${holiday}, $[name]`)
+  console.log(`${holiday}, ${name}`)
   return `Happy ${holiday}, ${name}!`
 }
 
